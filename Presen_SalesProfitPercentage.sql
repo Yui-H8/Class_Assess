6fd167229499152384ac13297efdf947981b9c8c
Yui <dataidm8@gmail.com>
2025-04-24 14:39:36 -0700

    Presen_SalesProfitPercentage
    -- Page 5 , 01 About Company 
    -- Added total and % , 
    -- Each year

--- Presen_SalesProfitPercentage.sql
+++ Presen_SalesProfitPercentage.sql
@@ -1,63 +1,56 @@
------------------------------------
--- This is for Powerpoint sql
------------------------------------
-USE [AdventureWorks2016]
-GO
-
--- (d)
--- Page 5 , 01 About Company
--- Added total and %
--- Create table / Code include 'DROP'
+ -- (d)
+-- Page 5 , 01 About Company 
+-- Added total and % , 
+-- Each year
 
 with
   ProfitsPerCoutry_CTE
-  as
-  (
-    select SUM(sod.LineTotal) - (pp.StandardCost * sum(sod.OrderQty)) AS TotalProfit, pcr.Name,
+as
+(
+    select pcr.Name,
+    YEAR(soh.Orderdate) AS 'Year',
+    SUM(sod.LineTotal) - (pp.StandardCost * sum(sod.OrderQty)) AS TotalProfit,
+    SUM(SubTotal) AS TotalSales
 
-      SUM(SubTotal) AS TotalSales
-
-    from Sales.SalesOrderDetail sod
-      join Production.Product pp
-      on pp.ProductID=sod.ProductID
-      join Sales.SalesOrderHeader soh
-      on soh.SalesOrderID=sod.SalesOrderID
-      join Sales.Customer c
-      on c.CustomerID=soh.CustomerID
-      join Person.person p
-      on c.PersonID=p.BusinessEntityID
-      join Person.Address pa
-      on pa.AddressID=soh.ShipToAddressID
-      join Person.StateProvince psp
-      on psp.StateProvinceID=pa.StateProvinceID
-      join Person.CountryRegion pcr
-      on psp.CountryRegionCode=pcr.CountryRegionCode
-    group by sod.ProductID,pp.StandardCost,pcr.Name
+from Sales.SalesOrderDetail sod
+    join Production.Product pp
+    on pp.ProductID=sod.ProductID
+    join Sales.SalesOrderHeader soh
+    on soh.SalesOrderID=sod.SalesOrderID
+    join Sales.Customer c
+    on c.CustomerID=soh.CustomerID
+    join Person.person p
+    on c.PersonID=p.BusinessEntityID
+    join Person.Address pa
+    on pa.AddressID=soh.ShipToAddressID
+    join Person.StateProvince psp
+    on psp.StateProvinceID=pa.StateProvinceID
+    join Person.CountryRegion pcr
+    on psp.CountryRegionCode=pcr.CountryRegionCode
+group by YEAR(soh.Orderdate),sod.ProductID,pp.StandardCost,pcr.Name
   )
 
 
-select Name, sum(TotalProfit) as profits,
+select [Year], sum(TotalProfit) as profits,
+    [Name],
+    SUM(TotalSales) AS AmountSales,
+    SUM(SUM(TotalSales)) OVER (PARTITION BY [Year]) AS AllofSales,
+    SUM(sum(TotalProfit)) OVER (PARTITION BY [Year]) AS AllofProfits
 
-  SUM(TotalSales) AS AmountSales,
-  SUM(SUM(TotalSales)) OVER () AS AllofSales,
-  SUM(sum(TotalProfit)) OVER () AS AllofProfits
-
-INTO SalesTable0413
+INTO SalesTableYear
 
 from ProfitsPerCoutry_CTE
-group by Name
-ORDER BY AmountSales DESC
+group by [Year], Name
+ORDER BY [Year],[Name],AmountSales DESC
+;
 
 -------
-DROP TABLE SalesTable0413;
--------
 
-SELECT Name, profits,
-  ( profits / AllofProfits ) * 100 AS PercentageofProfits,
-  AmountSales,
-  AllofSales,
-  ( AmountSales / AllofSales ) * 100 AS PercentageofSales
-FROM SalesTable0413
+SELECT [Name], [YEAR], profits,
+    ( profits / AllofProfits ) * 100 AS PercentageofProfits,
+    AmountSales,
+    AllofSales,
+    ( AmountSales / AllofSales ) * 100 AS PercentageofSales
+FROM SalesTableYear
 
-
------------------
+ORDER BY [YEAR],[Name]
